1a0a43eb39013b4154c60ddbd14ca1965a4ec74d
Michael Saiba <michaelsaiba84@gmail.com>
2022-11-08 04:23:31 +0300

    2nd hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Header from '../partials/Header';
 import HeroHome from '../partials/HeroHome';
+import HeroCustom from '../partials/HeroCustom';
 import FeaturesHome from '../partials/Features';
 import FeaturesBlocks from '../partials/FeaturesBlocks';
 import Testimonials from '../partials/Testimonials';
 import Newsletter from '../partials/Newsletter';
 import Footer from '../partials/Footer';
-import Banner from '../partials/Banner';
 
 function Home() {
   return (
@@ -20,6 +20,7 @@ function Home() {
       <main className="flex-grow">
 
         {/*  Page sections */}
+        <HeroCustom />
         <HeroHome />
         <FeaturesHome />
         <FeaturesBlocks />
